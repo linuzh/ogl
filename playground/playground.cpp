d78f0cca7b82a45cc19d8145a1b08cfa7173f1ee
linuzh <linuz@centrum.cz>
2019-01-27 23:45:55 +0100

    Tut 03
    shader and how use it

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <GL/glew.h>
+#include <GL/glew.h> // The OpenGL Extension Wrangler Library
 
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h> // A library for OpenGL, window and input
 GLFWwindow* window;
 
-#include <glm/glm.hpp>
+#include <glm/glm.hpp> // 3D mathematics
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
+
+#include <common/shader.hpp>
 
 int main( void )
 {
@@ -27,7 +30,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
+	window = glfwCreateWindow( 800, 600, "Playground", NULL, NULL); // glfwGetPrimaryMonitor()
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
@@ -44,16 +47,50 @@ int main( void )
 		return -1;
 	}
 
+	glm::mat4 myMatrix = glm::translate(glm::mat4(), glm::vec3(10.0f, 0.0f, 0.0f));
+	glm::vec4 MyVesctor(10.0f, 10.0f, 10.0f, 0.0f);
+	glm::vec4 transformedVector = myMatrix * MyVesctor;
+
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	// Dark gray background
+	glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
 
 	do{
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Draw nothing, see you in tutorial 2 !
+		glUseProgram(programID);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
